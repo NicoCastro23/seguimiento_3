205804beacf92cf7c49dc5cf3f8934211e5009f5
nicolas <jnch2005@gmail.com>
2025-02-14 17:41:42 -0500

    Solucion #10

---
+++ src/main/java/co/edu/uniquindio/poo/SumarDiagonal.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.poo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+//10
+public class SumarDiagonal {
+
+    public static int SumaDiagonal(int[][] matriz, int index) {
+        if (index >= matriz.length) { // caso base
+            return 0;
+        }
+
+        return matriz[index][index] + SumaDiagonal(matriz, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        int resultado = SumaDiagonal(matriz, 0);
+        System.out.println("La suma total de la diagonal es: " + resultado);
+    }
+}
